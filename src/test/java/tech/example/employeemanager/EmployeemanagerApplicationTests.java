40f704e32b6cd74f0dcfc61cde354121c0201858
aurora <aurorahoxha23@gmail.com>
2021-11-03 15:57:48 +0100

    Initial commit

---
+++ src/test/java/tech/example/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.example.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
